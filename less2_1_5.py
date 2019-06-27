5b157eaa275fb1778afb81669c9ed40e4b49d996
Екатерина Иванова <ivanekatrom@gmail.com>
2019-06-27 12:47:07 +0300

    adding code files

---
+++ less2_1_5.py
@@ -0,0 +1,25 @@
+﻿from selenium import webdriver
+import math
+
+
+link = "http://suninjuly.github.io/math.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+x_element = browser.find_element_by_id("input_value")
+x = x_element.text
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+y = calc(x)
+
+input1 = browser.find_element_by_id("answer")
+input1.send_keys(y)
+
+option1 = browser.find_element_by_id("robotCheckbox")
+option1.click()
+
+option2 = browser.find_element_by_id("robotsRule")
+option2.click()
+
+button = browser.find_element_by_css_selector("button.btn")
+button.click()
---
+++ less2_1_7.py
@@ -0,0 +1,27 @@
+﻿from selenium import webdriver
+import math
+
+
+link = "http://suninjuly.github.io/get_attribute.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+x_element1 = browser.find_element_by_id("treasure")
+x = x_element1.get_attribute("valuex")
+
+#x = x_element.text
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+y = calc(x)
+
+input1 = browser.find_element_by_id("answer")
+input1.send_keys(y)
+
+option1 = browser.find_element_by_id("robotCheckbox")
+option1.click()
+
+option2 = browser.find_element_by_id("robotsRule")
+option2.click()
+
+button = browser.find_element_by_css_selector("button.btn")
+button.click()
